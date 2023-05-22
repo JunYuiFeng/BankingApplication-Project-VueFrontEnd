d79c9c7cea7cc894a547ba5b0977ec985dd8be57
Alessandra Ribeiro <689693@student.inholland.nl>
2023-05-22 19:17:19 +0200

    trying to figure out the certification auth for htpps
    trying to figure out the certification auth for htpps

--- src/Axios-auth.js
+++ src/Axios-auth.js
@@ -1,5 +1,18 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: "http://localhost",
+  baseURL: "https://localhost:8443",
 });
 export default instance;
+/*
+import axios from "axios";
+import https from "https";
+
+const instance = axios.create({
+  httpsAgent: new https.Agent({
+    rejectUnauthorized: false,
+  }),
+  baseURL: "https://localhost:8443",
+});
+
+export default instance;
+*/
