5ab347e4827f6dddd0d61155645aff83d4806ee1
Alessandra Ribeiro <689693@student.inholland.nl>
2023-06-07 23:36:04 +0200

    merged conflicts
    merged conflicts

--- src/components/bankaccountsmanagement/BankAccountsManagement.vue
+++ src/components/bankaccountsmanagement/BankAccountsManagement.vue
@@ -25,49 +25,15 @@
       />
       <label class="btn btn-outline-primary me-2" for="radio2">Active</label>
 
-
-            <input type="radio" class="btn-check" name="options" id="radio3" autocomplete="off"
-                @change="getInactiveBankAccounts">
-            <label class="btn btn-outline-primary me-2" for="radio3">Inactive</label>
-        </div>
-
-        <div class="bankAccountsListContainer p-4 mt-5">
-            <div class="header p-3 mb-2">
-                <div class="row">
-                    <div class="col d-flex justify-content-center">
-                        <h5>name</h5>
-                    </div>
-                    <div class="col d-flex justify-content-center">
-                        <h5>account type</h5>
-                    </div>
-                    <div class="col d-flex justify-content-center">
-                        <h5>IBAN</h5>
-                    </div>
-                    <div class="col d-flex justify-content-center">
-                        <h5>absolute limit</h5>
-                    </div>
-                    <div class="col-2 d-flex justify-content-center">
-                        <h5>balance</h5>
-                    </div>
-                    <div class="col-1 d-flex justify-content-center">
-                        <h5>status</h5>
-                    </div>
-                    <div class="col d-flex justify-content-center">
-                        <h5>actions</h5>
-                    </div>
-                </div>
-            </div>
-            <div v-if="bankAccounts.length === 0">
-                <p class="d-flex justify-content-center mt-5 mb-5">No bank accounts available</p>
-            </div>
-            <div v-else-if="errorMessage">
-                <p class="d-flex justify-content-center mt-5 mb-5">{{ errorMessage }}</p>
-            </div>
-            <div v-else>
-                <BankAccount v-for="bankAccount in bankAccounts" :key="bankAccount.iban" :bankAccount="bankAccount" />
-            </div>
-        </div>
-
+      <input
+        type="radio"
+        class="btn-check"
+        name="options"
+        id="radio3"
+        autocomplete="off"
+        @change="getInactiveBankAccounts"
+      />
+      <label class="btn btn-outline-primary me-2" for="radio3">Inactive</label>
     </div>
 
     <div class="bankAccountsListContainer p-4 mt-5">
@@ -96,23 +62,23 @@
           </div>
         </div>
       </div>
-      <template v-if="bankAccounts.length === 0">
+      <div v-if="bankAccounts.length === 0">
         <p class="d-flex justify-content-center mt-5 mb-5">
           No bank accounts available
         </p>
-      </template>
-      <template v-else-if="errorMessage">
+      </div>
+      <div v-else-if="errorMessage">
         <p class="d-flex justify-content-center mt-5 mb-5">
           {{ errorMessage }}
         </p>
-      </template>
-      <template v-else>
+      </div>
+      <div v-else>
         <BankAccount
           v-for="bankAccount in bankAccounts"
           :key="bankAccount.iban"
           :bankAccount="bankAccount"
         />
-      </template>
+      </div>
     </div>
   </div>
 </template>
@@ -122,68 +88,23 @@ import axios from "../../Axios-auth";
 import BankAccount from "./BankAccount.vue";
 
 export default {
-
-    name: 'BankAccountsManagement',
-    components: {
-        BankAccount
-    },
-    data() {
-        return {
-            bankAccounts: [],
-            errorMessage: '',
-        }
-    },
-    mounted() {
-	    if (localStorage.getItem("role") !== "ROLE_EMPLOYEE") {
-      this.$router.push("/Overview");}
-        this.getBankAccounts();
-    },
-    methods: {
-        getBankAccounts() {
-            this.errorMessage = "";
-
-            axios.get('/BankAccounts/ExcludeUserAccount/1')
-                .then(response => {
-                    this.bankAccounts = response.data;
-                })
-                .catch(error => {
-                    console.log(error);
-                })
-        },
-        getActiveBankAccounts() {
-            this.errorMessage = "";
-
-            axios.get('/BankAccounts?status=active')
-                .then(response => {
-                    this.bankAccounts = response.data;
-                })
-                .catch(error => {
-                    console.log(error);
-                })
-        },
-        getInactiveBankAccounts() {
-            this.errorMessage = "";
-
-            axios.get('/BankAccounts?status=inactive')
-                .then(response => {
-                    this.bankAccounts = response.data;
-                })
-                .catch(error => {
-                    if (error.response && error.response.data && error.response.data.message) {
-                        const errorMessage = error.response.data.message;
-
-                        this.errorMessage = errorMessage;
-                    } else {
-                        console.log(error); // Fallback to logging the error if the message is not available
-                    }
-                });
-        }
-
+  name: "BankAccountsManagement",
+  components: {
+    BankAccount,
+  },
+  data() {
+    return {
+      bankAccounts: [],
+      errorMessage: "",
+    };
+  },
+  mounted() {
+    if (localStorage.getItem("role") !== "ROLE_EMPLOYEE") {
+      this.$router.push("/Overview");
     }
     if (!localStorage.getItem("token")) {
       this.$router.push("/login");
     }
-
     this.getBankAccounts();
   },
   methods: {
@@ -191,7 +112,7 @@ export default {
       this.errorMessage = "";
 
       axios
-        .get("/BankAccounts/ExceptBank")
+        .get("/BankAccounts/ExcludeUserAccount/1")
         .then((response) => {
           this.bankAccounts = response.data;
         })
@@ -213,7 +134,6 @@ export default {
     },
     getInactiveBankAccounts() {
       this.errorMessage = "";
-
       axios
         .get("/BankAccounts?status=inactive")
         .then((response) => {
--- src/components/overview/Overview.vue
+++ src/components/overview/Overview.vue
@@ -3,37 +3,36 @@
     <div class="container mt-5">
       <h1>Overview</h1>
 
+      <div class="d-flex justify-content-between mt-3">
+        <h5>Daylimit remaining: {{ getDayLimitRemaining }}</h5>
 
-            <div class="d-flex justify-content-between mt-3">
-                <h5>Daylimit remaining: {{ getDayLimitRemaining }}</h5>
+        <div>
+          <div
+            class="d-flex justify-content-end mb-2"
+            @click="this.$router.push('/CreateTransaction')"
+          >
+            <button type="button" class="btn btn-primary shadow-sm">
+              Transfer
+            </button>
+          </div>
 
-                <div>
-                    <div class="d-flex justify-content-end mb-2" @click="this.$router.push('/CreateTransaction')">
-                        <button type="button" class="btn btn-primary shadow-sm">Transfer</button>
-                    </div>
-
-                    <!-- <div class="d-flex justify-content-end">
+          <!-- <div class="d-flex justify-content-end">
                         <button type="button" class="btn btn-warning shadow-sm"
                             @click="showCreateBankAccount = true">Request
                             new
                             bank
                             account</button>
                     </div> -->
-                </div>
-            </div>
+        </div>
+      </div>
 
-            <!-- <CreateBankAccount v-if="showCreateBankAccount" @cancel="showCreateBankAccount = false" /> -->
+      <!-- <CreateBankAccount v-if="showCreateBankAccount" @cancel="showCreateBankAccount = false" /> -->
 
-            <div class="bankAccountsOverviewContainer ps-5 pe-5">
-                <div class="d-flex justify-content-end mt-5">
-                    <h5 class="">Total balance: €{{ formatNumber(calculateTotalBalance()) }}</h5>
-                </div>
-
-                <LoggedInUserBankAccount v-for="bankAccount in bankAccounts" :key="bankAccount.id"
-                    :bankAccount="bankAccount" />
-
-            </div>
-
+      <div class="bankAccountsOverviewContainer ps-5 pe-5">
+        <div class="d-flex justify-content-end mt-5">
+          <h5 class="">
+            Total balance: €{{ formatNumber(calculateTotalBalance()) }}
+          </h5>
         </div>
 
         <LoggedInUserBankAccount
@@ -46,75 +45,74 @@
   </div>
 </template>
 
-
-import axios from '../../Axios-auth';
+<script>
+import axios from "../../Axios-auth";
 import CreateBankAccount from "./RequestBankAccount.vue";
-import LoggedInUserBankAccount from './LoggedInUserBankAccount.vue'
+import LoggedInUserBankAccount from "./LoggedInUserBankAccount.vue";
 import { useUserSessionStore } from "../../store/userSessionStore";
 
-
 export default {
-    setup() {
-        return { store: useUserSessionStore() };
-    },
-    components: {
-        LoggedInUserBankAccount
-    },
-    data() {
-        return {
-            bankAccounts: [],
-            loggedInUser: {},
-            showCreateBankAccount: false
-        }
-    },
-    mounted() {
-	 if (!localStorage.getItem("token")) {
+  setup() {
+    return { store: useUserSessionStore() };
+  },
+  components: {
+    LoggedInUserBankAccount,
+  },
+  data() {
+    return {
+      bankAccounts: [],
+      loggedInUser: {},
+      showCreateBankAccount: false,
+    };
+  },
+  mounted() {
+    if (!localStorage.getItem("token")) {
       this.$router.push("/login");
     }
-        this.getLoggedInUserBankAccounts();
-        this.getLoggedInUser();
+    this.getLoggedInUserBankAccounts();
+    this.getLoggedInUser();
+  },
+  methods: {
+    getLoggedInUserBankAccounts() {
+      axios
+        .get(`/BankAccounts/UserAccount/${this.store.getUserId}`)
+        .then((response) => {
+          this.bankAccounts = response.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     },
-    methods: {
-        getLoggedInUserBankAccounts() {
-            axios.get(`/BankAccounts/UserAccount/${this.store.getUserId}`)
-                .then((response) => {
-                    this.bankAccounts = response.data;
-                })
-                .catch((error) => {
-                    console.log(error);
-                })
-        },
-        calculateTotalBalance() {
-            return this.bankAccounts.reduce((total, bankAccount) => {
-                return total + bankAccount.balance;
-            }, 0);
-        },
-        getLoggedInUser(){
-            axios.get(`/UserAccounts/${this.store.getUserId}`)
-                .then((response) => {
-                    this.loggedInUser = response.data;
-                })
-                .catch((error) => {
-                    console.log(error);
-                })
-        },
-        formatNumber(number) {
-            const parts = number.toString().split('.');
-            parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, '.');
-            return parts.join(',');
-        },
+    calculateTotalBalance() {
+      return this.bankAccounts.reduce((total, bankAccount) => {
+        return total + bankAccount.balance;
+      }, 0);
     },
-    computed: {
-        getDayLimitRemaining() {
-            if (this.loggedInUser != null) {
-                return this.loggedInUser.dayLimit - this.loggedInUser.currentDayLimit;
-            }
-            return 0;
-        },
-    }
-}
-
-
+    getLoggedInUser() {
+      axios
+        .get(`/UserAccounts/${this.store.getUserId}`)
+        .then((response) => {
+          this.loggedInUser = response.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    formatNumber(number) {
+      const parts = number.toString().split(".");
+      parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+      return parts.join(",");
+    },
+  },
+  computed: {
+    getDayLimitRemaining() {
+      if (this.loggedInUser != null) {
+        return this.loggedInUser.dayLimit - this.loggedInUser.currentDayLimit;
+      }
+      return 0;
+    },
+  },
+};
 </script>
 
 <style scoped>
